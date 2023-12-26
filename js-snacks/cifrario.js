de33b152e382dd93d5dacba4dd5582476791d434
MicheleSerafini <serafini.michele01@gmail.com>
2023-12-26 10:38:43 +0100

    ADD > Cifrario farfallino in js

---
+++ js-snacks/cifrario.js
@@ -0,0 +1,39 @@
+// creo una funzione per verificare se la lettera corrente sia una vocale
+function isVowel(char) {
+  const vowels = ["a", "e", "i", "o", "u"];
+
+  for (let i = 0; i < vowels.length; i++) {
+    const vowel = vowels[i];
+
+    if (vowel === char) {
+      return true;
+    }
+  }
+
+  return false;
+
+  /**
+   * \/\/\/ versione semplificata con il metodo includes \/\/\/
+   * return ['a', 'e', 'i', 'o', 'u'].includes(char);
+   */
+}
+
+function getStrIntoFarfallino(randomString) {
+  // dichiaro una variabile per la stringa farfallina
+  let rdmStringFar = "";
+
+  // creo un ciclo per analizzare ogni lettera della stringa random
+  for (let i = 0; i < randomString.length; i++) {
+    const letter = randomString[i];
+
+    if (isVowel(letter)) {
+      rdmStringFar += letter + "f" + letter;
+    } else {
+      rdmStringFar += letter;
+    }
+  }
+
+  return rdmStringFar;
+}
+const rdmWord = "marco";
+console.log(getStrIntoFarfallino(rdmWord));
---
+++ js-snacks/readme.md
@@ -0,0 +1,9 @@
+> Data una stringa, restituire la versione in alfabeto farfallino (le vocali devono essere trasformate in questo modo: a -> afa, e -> efe, i -> ifi, o -> ofo, u- > ufu)
+
+> stringa = “marco”; // risultato mafarcofo
+
+- data una stringa
+- memorizzo in un array le vocali
+- vado a ciclare la stringa
+- durante il ciclo vado a confrontare le lettere, se corrispondono aggiungo una stringa con "f" + la vocale corrente della stringa
+- restituisco alla fine la stringa farfalinizzata
